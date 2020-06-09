c15439449a585ef31f55e46c9584c711323b1c02
Anne Richardson <richardson.ae@gmail.com>
2020-06-09 07:17:04 -0500

    Add games relationship

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root to: 'games#index'
+  root 'games#index'
 
   # Skip registrations for now so no new users can sign up.
   devise_for :users, skip: [:registrations]
