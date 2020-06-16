03175e4e6bb9e41e8556810177898cd309e200b5
Anne Richardson <richardson.ae@gmail.com>
2020-06-16 18:25:48 -0500

    Allow users to sign up with game code

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,18 +1,18 @@
 # frozen_string_literal: true
 
 class PlayersController < ApplicationController
-  before_action :set_game, only: %i[index new create]
+  before_action :set_game, only: %i[index create]
 
   def index
     @players = @game.players
   end
 
   def new
-    @player = @game.players.new
+    @player = Player.new
   end
 
   def create
-    @player = @game.players.new(player_params)
+    @player = @game.players.new(player_params.except(:game_code))
 
     if @player.save
       redirect_to game_players_url(@game)
@@ -24,10 +24,14 @@ class PlayersController < ApplicationController
   private
 
   def set_game
-    @game = Game.find(params[:game_id])
+    if params[:game_id].present?
+      @game = Game.find_by(id: params[:game_id])
+    else
+      @game = Game.find_by(code: player_params[:game_code]&.upcase)
+    end
   end
 
   def player_params
-    params.require(:player).permit(:game_id, :name)
+    params.require(:player).permit(:game_id, :name, :game_code)
   end
 end
