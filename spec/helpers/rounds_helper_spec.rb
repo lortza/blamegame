2712cf6fde149ca28a51ccb8e3df700ed48546be
Anne Richardson <richardson.ae@gmail.com>
2020-06-23 16:20:32 -0500

    Add specs for display_winner

--- spec/helpers/rounds_helper_spec.rb
+++ spec/helpers/rounds_helper_spec.rb
@@ -3,7 +3,6 @@
 require 'rails_helper'
 
 RSpec.describe RoundsHelper, type: :helper do
-
   let(:game) { create(:game) }
 
   before do
@@ -11,7 +10,6 @@ RSpec.describe RoundsHelper, type: :helper do
   end
 
   describe 'next_round_submission' do
-
     it 'returns a new submission link' do
       round = game.rounds.first
       output = helper.next_round_submission(round)
@@ -26,7 +24,25 @@ RSpec.describe RoundsHelper, type: :helper do
   end
 
   describe 'display_winner' do
-    let!(:game) { create(:game) }
+    let(:game) { create(:game) }
+    let(:round) { create(:round, game: game) }
+    let(:player) { create(:player, game: game, name: 'lorem') }
 
+    it 'displays the round winner name' do
+      round
+      allow_any_instance_of(Round).to receive(:winner).and_return(player)
+      expect(display_winner(round)).to eq(player.name)
+    end
+
+    it 'displays the game winner name' do
+      game
+      allow_any_instance_of(Game).to receive(:winner).and_return(player)
+      expect(display_winner(game)).to eq(player.name)
+    end
+
+    it 'displays a tie message if there is no winner' do
+      allow_any_instance_of(Game).to receive(:winner).and_return([])
+      expect(display_winner(game).downcase).to include('tie')
+    end
   end
 end
