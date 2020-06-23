559bbe99f8b81ce69972e94900e100b8eb20a58d
Anne Richardson <richardson.ae@gmail.com>
2020-06-23 08:29:43 -0500

    Handle missing player ids

--- app/helpers/submissions_helper.rb
+++ app/helpers/submissions_helper.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-module PlayersHelper
+module SubmissionsHelper
   def players_minus_nominator(game, nominator)
-    game.players.where.not(id: nominator.id)
+    game.players.where.not(id: nominator&.id)
   end
 end
