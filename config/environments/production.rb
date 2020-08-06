557fbb69e7b10b0f6ef4b921704750acf25e2349
Anne Richardson <richardson.ae@gmail.com>
2020-08-05 19:05:05 -0500

    Rename allthethings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "action_cable_rails_six_production"
+  # config.active_job.queue_name_prefix = "voting_game_fresh_production"
 
   config.action_mailer.perform_caching = false
 
