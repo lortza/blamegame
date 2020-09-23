cb521b92aebea4e5181bd8ef37c0c5b83af5ac97
Anne Richardson <richardson.ae@gmail.com>
2020-09-22 19:06:03 -0500

    Redis config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,9 +39,9 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Mount Action Cable outside main process or domain.
-  # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  # config.action_cable.mount_path = '/cable'
+  config.action_cable.url = 'wss://blamegame.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = [ 'https://blamegame.herokuapp.com', 'http://blamegame.herokuapp.com', /http:\/\/blamegame.herokuapp.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
