9d0c25dcccae44eeccb333c7c41aa90d9c8ce85c
Anne Richardson <richardson.ae@gmail.com>
2020-09-24 10:25:48 -0500

    Remove messages and room assets from tutorial

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -5,11 +5,23 @@
 # Table name: submissions
 #
 #  id           :bigint           not null, primary key
-#  round_id     :bigint           not null
-#  candidate_id :bigint
-#  voter_id     :bigint
 #  created_at   :datetime         not null
 #  updated_at   :datetime         not null
+#  candidate_id :bigint
+#  round_id     :bigint           not null
+#  voter_id     :bigint
+#
+# Indexes
+#
+#  index_submissions_on_candidate_id  (candidate_id)
+#  index_submissions_on_round_id      (round_id)
+#  index_submissions_on_voter_id      (voter_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (candidate_id => players.id)
+#  fk_rails_...  (round_id => rounds.id)
+#  fk_rails_...  (voter_id => players.id)
 #
 class Submission < ApplicationRecord
   belongs_to :round
