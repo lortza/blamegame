4f918a26aafc2e21fcbbd51daae45ed37b8e2da9
Anne Richardson <richardson.ae@gmail.com>
2020-09-29 19:51:23 -0500

    Rake task, seeds, and schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_24_151733) do
+ActiveRecord::Schema.define(version: 2020_09_29_002205) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "decks", force: :cascade do |t|
+    t.string "name"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_decks_on_user_id"
+  end
 
   create_table "games", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -39,6 +47,8 @@ ActiveRecord::Schema.define(version: 2020_09_24_151733) do
     t.boolean "adult_rating"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "deck_id"
+    t.index ["deck_id"], name: "index_questions_on_deck_id"
   end
 
   create_table "rounds", force: :cascade do |t|
@@ -76,8 +86,10 @@ ActiveRecord::Schema.define(version: 2020_09_24_151733) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "decks", "users"
   add_foreign_key "games", "users"
   add_foreign_key "players", "games"
+  add_foreign_key "questions", "decks"
   add_foreign_key "rounds", "games"
   add_foreign_key "rounds", "questions"
   add_foreign_key "submissions", "players", column: "candidate_id"
